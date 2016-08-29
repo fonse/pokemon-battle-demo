73ee2765cdb80af709061c5bd1eefec050697429
DurandA <arnaud.durand@unifr.ch>
2016-08-29 16:35:54 +0200

    Add JSON parser

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.json());
 
 app.get('/', function(request, response) {
   var names = require(__dirname + '/data/names.json');
